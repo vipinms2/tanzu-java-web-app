549e41e548e9083139d92d9b3d199d3eedacb760
vipinms2 <104124144+vipinms2@users.noreply.github.com>
2022-04-28 14:47:55 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! 25 apr 2022 10.07 pm 26 apr";
+		return "Greetings from Spring Boot + Tanzu! 2.47 pm 28 apr";
 	}
 
 }
